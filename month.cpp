f8f7a0cc87f99054b5c6a3c1b8565ef54b865dac
Noah Gaffney <35945244+NoahGaff@users.noreply.github.com>
2018-08-31 18:54:51 -0400

    Create month.cpp

---
+++ month.cpp
@@ -0,0 +1,45 @@
+/*
+Author: Noah Gaffney
+Course: CSCI-136
+Instructor: I don't rememeber right now
+Assignment: smaller
+tells you if its a leap year
+*/
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    cout << "What is the year?" << endl;
+    int year = 0;
+    cin >> year;
+    
+    cout << endl;
+    
+    cout << "What is the month?" << endl;
+    int year = 0;
+    cin >> month;
+    
+    cout << endl;
+    
+    int daysInMonth = 0; 
+    
+    if ((month > 7) && (month % 2 == 0) || (month < 7) && (month % 2 != 0) && (month != 2)) 
+      daysInMonth = 30; 
+      
+    else if month = 2
+    {
+      if (year % 4 != 0) {daysInMonth = 28}
+      else if (year % 100 != 0) {daysInMonth = 29}
+      else if (year % 400 != 0) {daysInMonth = 28}
+      else {daysInMonth = 29}
+    }
+    
+    else 
+      daysInMonth = 31
+      
+    cout << daysInMonth
+    
+    return 0;
+}
