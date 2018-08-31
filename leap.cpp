6b3e79a16ee8ed6c75c293cce735b1233aa8eaef
Noah Gaffney <35945244+NoahGaff@users.noreply.github.com>
2018-08-31 18:38:57 -0400

    Update leap.cpp

--- leap.cpp
+++ leap.cpp
@@ -24,10 +24,10 @@ int main ()
     else (it is a leap year)
     */
     
-    if (year % 4 != 0) {cout << "Common year"}
-    else if (year % 100 != 0) {cout << "Leap year"}
-    else if (year % 400 != 0) {cout << "Common year"}
-    else {cout << "Leap year"}
+    if (year % 4 != 0) {cout << "Common year";}
+    else if (year % 100 != 0) {cout << "Leap year";}
+    else if (year % 400 != 0) {cout << "Common year";}
+    else {cout << "Leap year";}
     
     return 0;
 }
