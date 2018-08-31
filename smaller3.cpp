6782378b54390d9830968f6c4305886737aca22b
Noah Gaffney <35945244+NoahGaff@users.noreply.github.com>
2018-08-31 18:30:50 -0400

    Update smaller3.cpp

--- smaller3.cpp
+++ smaller3.cpp
@@ -30,9 +30,9 @@ int main ()
     
     cout << endl;
     
-    if (num1 < num2) & (num1 < num3)
+    if ((num1 < num2) && (num1 < num3))
         cout << num1 << " is smaller";
-    else if (num2 < num3) & (num2 < num3)
+    else if ((num2 < num3) && (num2 < num3))
         cout << num2  << " is smaller";
     else 
         cout << num3  << " is smaller";
