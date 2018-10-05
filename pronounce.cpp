b026f8ba1539dd9369534966d6409f84a331282b
Noah Gaffney <35945244+NoahGaff@users.noreply.github.com>
2018-10-05 18:39:29 -0400

    Update pronounce.cpp

--- pronounce.cpp
+++ pronounce.cpp
@@ -6,7 +6,7 @@
  
  Gives the prununciation of
  input words
- */
+
 
 
 #include <iostream>
@@ -61,3 +61,76 @@ int main()
     return 0;
     
 }
+*/
+
+/*
+ Author: Noah Gaffney
+ Course: CSCI-135
+ Instructor: their name
+ Assignment: Project 2: Part I
+ 
+ Gives the prununciation of
+ input words
+ okay
+ */
+
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void splitOnSpace(string s, string & before, string & after)
+{
+    // reset strings
+    before = "";
+    after = "";
+    // accumulate before space
+    int i = 0;
+    while (i < s.size() && not isspace(s[i])) { before += s[i]; i++; }
+    // skip the space
+    i++;
+    // accumulate after space
+    while (i < s.size()) { after += s[i]; i++; }
+}
+
+string findWord(string inputWord)
+{
+    ifstream input;
+    string compareWord;
+    
+    input.open("cmudict.0.7a");
+    if(input.fail()){
+        cout << "File did not open";
+    }
+    
+    for(int i = 0; i < inputWord.size(); i++) { inputWord.at(i) = toupper(inputWord.at(i)); }
+    
+    while(input >> compareWord)
+    {
+        if(compareWord == inputWord)
+        {
+            getline(input, inputWord);
+            break;
+        }
+    }
+    
+    return compareWord;
+}
+
+int main()
+{
+    string inputWord, compareWord, afterSpace, beforeSpace;
+    
+    cin >> inputWord;
+    
+    compareWord = findWord(inputWord);
+    
+    splitOnSpace(compareWord + inputWord, beforeSpace, afterSpace);
+    splitOnSpace(compareWord + inputWord, beforeSpace, afterSpace);
+    
+    if (afterSpace == "") { cout << "Not found"; }
+    else { cout << "Pronounciation: " << afterSpace << endl; }
+
+    
+}
+
