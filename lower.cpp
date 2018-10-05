a008f82d25bb3b998bd1b43aec84acd49f482940
Noah Gaffney <35945244+NoahGaff@users.noreply.github.com>
2018-10-04 20:29:09 -0400

    Create lower.cpp

---
+++ lower.cpp
@@ -0,0 +1,31 @@
+/*
+ Author: Noah Gaffney
+ Course: CSCI-135
+ Instructor: their name
+ Assignment: Project 2: Part I
+ 
+ Gives the prununciation of
+ input words
+ */
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int sideLength;
+    
+    cout << "Input side length: ";
+    cin >> sideLength;
+    
+    for (int i = 0; i < sideLength; i++)
+    {
+        for (int g = 0; g < i; g++)
+        {
+            cout << "*";
+        }
+        cout << endl << endl; 
+    }
+    
+    return 0;
+}
