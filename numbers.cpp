cc65c2195ea21721c87a2a9cc36eb59f424bb9a7
Noah Gaffney <35945244+NoahGaff@users.noreply.github.com>
2018-10-10 15:21:15 -0400

    Create numbers.cpp

---
+++ numbers.cpp
@@ -0,0 +1,33 @@
+/*
+Author: Noah Gaffney
+Course: CSCI-136
+Instructor: their name
+Assignment: Lab 5 A
+
+Here, briefly, at least in one or a few sentences
+describe what the program does.
+*/
+
+#include <iostream>
+using namespace std;
+
+bool isDivisibleBy(int n, int d)
+{
+   if (d == 0 || n % d != 0) {return flase;)
+   else (return true;)
+}
+
+int main() 
+{
+  int num1; 
+  cout << "Enter first number: " 
+  cin >> num1; 
+  
+  int num2; 
+  cout << "Enter second number: " 
+  cin >> num2; 
+  
+  cout << bool isDivisibleBy << endl; 
+  
+  return 0; 
+}
