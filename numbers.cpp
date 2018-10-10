860cf6933d19436b445f62c08758b4c18bf7c9d6
Noah Gaffney <35945244+NoahGaff@users.noreply.github.com>
2018-10-10 15:26:26 -0400

    Update numbers.cpp

--- numbers.cpp
+++ numbers.cpp
@@ -20,11 +20,11 @@ bool isDivisibleBy(int n, int d)
 int main() 
 {
   int num1; 
-  cout << "Enter first number: " 
+  cout << "Enter first number: ";
   cin >> num1; 
   
   int num2; 
-  cout << "Enter second number: " 
+  cout << "Enter second number: ";
   cin >> num2; 
   
   cout << bool isDivisibleBy << endl; 
