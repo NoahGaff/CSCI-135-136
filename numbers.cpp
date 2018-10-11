36a5d17fc33ed1e0d49b8c7a8b1727284067e0b6
Noah Gaffney <35945244+NoahGaff@users.noreply.github.com>
2018-10-11 17:35:54 -0400

    Update numbers.cpp

--- numbers.cpp
+++ numbers.cpp
@@ -1,68 +1,75 @@
 /*
-Author: Noah Gaffney
-Course: CSCI-136
-Instructor: their name
-Assignment: Lab 5 A
-
-Here, briefly, at least in one 
-or a few sentences
-describe what the 
-program does.
-*/
+ Author: Noah Gaffney
+ Course: CSCI-136
+ Instructor: their name
+ Assignment: Lab 5 A
+ Here, briefly, at least in one
+ or a few sentences
+ describe what the
+ program does.
+ */
 
 #include <iostream>
 using namespace std;
 
 bool isDivisibleBy(int n, int d)
 {
-   if (d == 0 || n % d != 0) {return false;}
-   else {return true;}
+    if (d == 0 || n % d != 0) {return false;}
+    else {return true;}
 }
 
 
 bool isPrime(int n)
 {
-   if (n <= 1) { return false; }
-   
-   for (int i = 2; i <= n-1; i++) 
-   { 
-      if (isDivisibleBy(n, i)) {return false;}
-   }
-   
-   return true;
+    if (n <= 1) { return false; }
+    
+    for (int i = 2; i <= n-1; i++)
+    {
+        if (isDivisibleBy(n, i)) {return false;}
+    }
+    
+    return true;
 }
 
 int nextPrime(int n)
 {
-   int next = n + 1;
-   while (! isPrime (next)) { next++; }
-   return next; 
+    int next = n + 1;
+    while (! isPrime (next)) { next++; }
+    return next;
 }
 
 int countPrimes(int a, int b)
 {
-   int numOfPrimes = 0; 
-   for (int i = a; i <= b; i++)
-   {
-      if (isPrime(i)) {numOfPrimes++;} 
-   }
-   
-   return numOfPrimes;
+    int numOfPrimes = 0;
+    for (int i = a; i <= b; i++)
+    {
+        if (isPrime(i)) {numOfPrimes++;}
+    }
+    
+    return numOfPrimes;
 }
-//
-//
-//what is wrong with gradescope
-int main() 
+
+bool isTwinPrime(int n)
 {
-  int num1; 
-  cout << "Enter first number: ";
-  cin >> num1; 
-   
-  int num2; 
-  cout << "Enter Second number: ";
-  cin >> num2; 
-  
-  cout << countPrimes(num1, num2) << endl; 
-  
-  return 0; 
+    int larger = n + 2;
+    int smaller = n - 2;
+    
+    if (isPrime(larger) || isPrime(smaller)) { return true; }
+    
+    return false;
+}
+
+int main()
+{
+    int num1;
+    cout << "Enter first number: ";
+    cin >> num1;
+    
+    //int num2;
+    //cout << "Enter Second number: ";
+    //cin >> num2;
+    
+    cout << isTwinPrime(num1) << endl;
+    
+    return 0;
 }
