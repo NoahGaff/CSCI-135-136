35e4ff36d2e4a16aecaf414eab64fc221b780558
Noah Gaffney <35945244+NoahGaff@users.noreply.github.com>
2018-10-11 17:13:23 -0400

    Update numbers.cpp

--- numbers.cpp
+++ numbers.cpp
@@ -38,6 +38,15 @@ int nextPrime(int n)
    return next; 
 }
 
+int countPrimes(int a, int b)
+{
+   int numOfPrimes = 0; 
+   for (int i = a; i <= b; i++)
+   {
+      if (isPrime(i)) {numOfPrimes++;} 
+   }
+}
+
 int main() 
 {
   int num1; 
